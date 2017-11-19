c4ecceb10b311380089b0efead09e904752b009c
Jay Shah <jayshah.9596@gmail.com>
2017-11-19 08:24:33 +0530

    sync

--- src/App.js
+++ src/App.js
@@ -52,96 +52,91 @@ class App extends Component {
         }
       });
     }
-    const data = {
-      "Starter-kit": {
-        "react-native-training-react-native-hackathon-starter": {
-          "id": "1",
-          "name": "react-native-hackathon-starter",
-          "url": "https://github.com/react-native-training/react-native-hackathon-starter",
-          "author": "react-native-training",
-          "star": "349",
-          "instructions": [
-            "npm install",
-            "react-native link"
-          ],
-          "tags": [
-            "react-native"
-          ]
-        },
-        "qdouble-angular-webpack2-starter": {
-          "id": "2",
-          "url": "https://github.com/qdouble/angular-webpack2-starter",
-          "name": "angular-webpack2-starter",
-          "author": "qdouble",
-          "star": "758",
-          "instructions": [
-            "cd angular-webpack2-starter",
-            "yarn"
-          ],
-          "tags": [
-            "angular",
-            "typescript"
-          ]
-        },
-        "antonybudianto-angular-starter": {
-          "id": "3",
-          "url": "https://github.com/antonybudianto/angular-starter",
-          "name": "angular-starter",
-          "author": "antonybudianto",
-          "star": "544",
-          "instructions": [
-            "npm install"
-          ],
-          "tags": [
-            "angular",
-            "typescript",
-            "gulp"
-          ]
-        },
-        "wellyshen-react-cool-starter": {
-          "id": "4",
-          "url": "https://github.com/wellyshen/react-cool-starter",
-          "name": "react-cool-starter",
-          "author": "wellyshen",
-          "star": "481",
-          "instructions": [
-            "yarn"
-          ],
-          "tags": [
-            "react",
-            "redux",
-            "express",
-            "jest",
-            "flow",
-            "react-router",
-            "webpack"
-          ]
-        }
-      }
+    const data = [{
+      "id": "1",
+      "name": "react-native-hackathon-starter",
+      "url": "https://github.com/react-native-training/react-native-hackathon-starter",
+      "author": "react-native-training",
+      "star": "349",
+      "instructions": [
+        "npm install",
+        "react-native link"
+      ],
+      "tags": [
+        "react-native"
+      ]
+    }, {
+      "id": "2",
+      "url": "https://github.com/qdouble/angular-webpack2-starter",
+      "name": "angular-webpack2-starter",
+      "author": "qdouble",
+      "star": "758",
+      "instructions": [
+        "cd angular-webpack2-starter",
+        "yarn"
+      ],
+      "tags": [
+        "angular",
+        "typescript"
+      ]
+    }, {
+      "id": "3",
+      "url": "https://github.com/antonybudianto/angular-starter",
+      "name": "angular-starter",
+      "author": "antonybudianto",
+      "star": "544",
+      "instructions": [
+        "npm install"
+      ],
+      "tags": [
+        "angular",
+        "typescript",
+        "gulp"
+      ]
+    }, {
+      "id": "4",
+      "url": "https://github.com/wellyshen/react-cool-starter",
+      "name": "react-cool-starter",
+      "author": "wellyshen",
+      "star": "481",
+      "instructions": [
+        "yarn"
+      ],
+      "tags": [
+        "react",
+        "redux",
+        "express",
+        "jest",
+        "flow",
+        "react-router",
+        "webpack"
+      ]
     }
-    return (
-      <div className="App">
-        <ListContainer
-          data={data}
-          handleKitClick={this.handleKitClick} />
+  ]
+  return(
+      <div className = "App" >
+      <ListContainer
+        data={data}
+        handleKitClick={this.handleKitClick} />
         {
           this.state.starterKitID === null ?
-            // (this.state.starterKitID === null || this.state.directory === null) ?
-            <MainContainer
-              // handlePlatformClick={this.handlePlatformClick}
+        // (this.state.starterKitID === null || this.state.directory === null) ?
+        <MainContainer
+          // handlePlatformClick={this.handlePlatformClick}
 
-              handleTechClick={this.handleTechClick}
-            /> :
-            <KitContainer
-              starterKitID={this.state.starterKitID}
-              handleKitClick={this.handleKitClick}
-              setDirectory={this.setDirectory}
-              directory={this.state.directory}
-            />
+          handleTechClick={this.handleTechClick}
+        /> :
+        <KitContainer
+          data = {data}
+          starterKitID={this.state.starterKitID}
+          handleKitClick={this.handleKitClick}
+          setDirectory={this.setDirectory}
+          directory={this.state.directory}
+        />
         }
       </div>
     );
-  }
+}
 }
 
 export default App;
